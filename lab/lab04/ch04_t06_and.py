4b5bbbc8472a56a879bc85ac59b899871c060501
WongYanKi-2627 <wongyenkiofficial@gmail.com>
2026-09-24 06:31:22 +0000

    [skip actions] lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -2,10 +2,10 @@ bool_one = False
 
 bool_two = False
 
-bool_three = 19 % 4 != 300 / 10 /10 and False
+bool_three = 19 % 4 != 300 / 10 / 10 and False
 
 bool_four = None
 
 bool_five = None
 
-print(bool_two)
+print(bool_three)
