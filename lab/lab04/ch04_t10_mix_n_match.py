498306e4b09900f438ee986c9f0a25d450a026f5
WongYanKi-2627 <wongyenkiofficial@gmail.com>
2026-09-24 08:00:10 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -9,7 +9,8 @@ bool_two = 1 == 1 and 3 > 1
 # Make me false!
 bool_three = 30 >= 1 and 10 < 1
 # Make me true!
-bool_four = not
+bool_four = not 20 > 1
 
 # Make me true!
 bool_five = (2 <= 2) and "Alpha" == "Bravo"
+F
